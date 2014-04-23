9e9667aaabb19202a2396114520b9e379d634e74
yailebs <yailebs@s4pc11.eisc.univalle.edu.co>
2014-04-23 15:35:21 -0500

    Agregado atributo ensayo

---
+++ Empleados/src/empleados/Observador.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package empleados;
+
+/**
+ *
+ * @author yailebs
+ */
+public interface Observador {
+    public void refreshData(Observable subject);
+}
